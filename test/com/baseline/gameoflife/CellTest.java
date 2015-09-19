4c08ea934a049d6ac8c1f12225a8c7d91947497c
kusumi <kusumi@INkusumi.local>
2015-09-19 13:09:55 +0530

    Made changes to rectify previous errors

--- test/com/baseline/gameoflife/CellTest.java
+++ test/com/baseline/gameoflife/CellTest.java
@@ -43,52 +43,52 @@ public class CellTest {
     }
 
     @Test
-    public void shouldReturnTrueIfTheCellIsDiagnolAndUpAndLeft() {
-        Cell cell = new Cell(0, 0);
+    public void shouldReturnTrueIfTheCellIsDiagonalAndUpAndLeft() {
+        Cell cell = new Cell(1, 1);
 
-        assertTrue(cell.isNeighbour(1, 1));
+        assertTrue(cell.isNeighbour(0, 0));
     }
 
     @Test
     public void shouldReturnTrueIfTheCellIsDiagnolAndUpAndRight() {
-        Cell cell = new Cell(0, 2);
+        Cell cell = new Cell(1, 1);
 
-        assertTrue(cell.isNeighbour(1, 1));
+        assertTrue(cell.isNeighbour(0, 2));
     }
 
     @Test
     public void shouldReturnTrueIfTheCellIsDiagnolAndBelowAndLeft() {
-        Cell cell = new Cell(2, 0);
+        Cell cell = new Cell(1, 1);
 
-        assertTrue(cell.isNeighbour(1, 1));
+        assertTrue(cell.isNeighbour(2, 0));
     }
 
     @Test
     public void shouldReturnTrueIfTheCellIsDiagnolAndBelowAndRight() {
-        Cell cell = new Cell(0, 2);
+        Cell cell = new Cell(1, 1);
 
-        assertTrue(cell.isNeighbour(1, 1));
+        assertTrue(cell.isNeighbour(0, 2));
     }
 
     @Test
     public void shouldReturnFalseIfTheCellIsNotANeighbourAndOnTheRowAboveOnTheRight() {
-        Cell cell = new Cell(0, 3);
+        Cell cell = new Cell(1, 1);
 
-        assertFalse(cell.isNeighbour(1, 1));
+        assertFalse(cell.isNeighbour(0, 3));
     }
 
     @Test
     public void shouldReturnFalseIfTheCellIsNotANeighbourAndOnTheRowAboveOnTheLeft() {
-        Cell cell = new Cell(0, 0);
+        Cell cell = new Cell(1, 2);
 
-        assertFalse(cell.isNeighbour(1, 2));
+        assertFalse(cell.isNeighbour(0, 0));
     }
 
     @Test
     public void shouldReturnFalseIfTheCellIsNotANeighbourAndOnTheBelowAboveOnTheRight() {
-        Cell cell = new Cell(2, 3);
+        Cell cell = new Cell(1, 1);
 
-        assertFalse(cell.isNeighbour(1, 1));
+        assertFalse(cell.isNeighbour(2, 3));
     }
 
     @Test
